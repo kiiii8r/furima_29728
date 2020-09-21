8e1e9bc2e200cd5ba159419b5d40fd02a893d251
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-09-22 00:38:23 +0900

    validate　一部

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nick_name, presence: true
+         validates :email, presence: true, uniqueness: true
+         validates :password, presence: true
+         validates :birth_day, presence: true
 end
