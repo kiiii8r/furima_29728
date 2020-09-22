18112872087563d536c47e2e3a8af1441da39ecd
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-09-22 15:02:58 +0900

    ルーティング作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
-  def edit
-    @user = Users.new
+  def index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  
-  resources :users, only: [:edit, update]
+  root to: "users#index"
+
+  resources :users, only: [:edit, :update]
 
 end
