288fce22cc322c200dd721ca394d95556e3a3f3a
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-09-24 18:40:13 +0900

    ActiveHash関連作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :day, :derively_fee, :prefecture, :status
+
+  validates :name, :explanation, :category, :derively_fee, :status, :prefecture, :day, :price, precence: { message:"can't be blank" }
+
+  validates :category_id, :day_id, :derively_fee_id, :prefecture_id, :status_id, numericality: { other_than: 1, message:"select" }
+
+  validetes :price, format: { with: /\A[0-9]+\z/, message: "Price Half-width number"}
 end
---
+++ spec/factories/derively_fees.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :derively_fee do
+    
+  end
+end
