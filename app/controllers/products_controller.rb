03e76aeaec33828b943f0d22c8dd4a1bf77649e0
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-09-27 21:51:03 +0900

    商品詳細表示機能の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new
+    @product = Product.new(product_params)
     if @product.save
       redirect_to root_path
     else
