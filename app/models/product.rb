0d38dc642ea119392cdb3697fb341aaea004b4d9
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-09-27 17:15:18 +0900

    コード整頓、可読性向上の為不要箇所削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,23 +10,23 @@ class Product < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-  validates :image
-  validates :name
-  validates :explanation
-  validates :category_id
-  validates :derively_fee_id
-  validates :status_id
-  validates :prefecture_id
-  validates :day_id
-  validates :price 
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :derively_fee_id
+    validates :status_id
+    validates :prefecture_id
+    validates :day_id
+    validates :price 
   end
 
   with_options numericality: { other_than: 0, message: "select" } do
-  validates :category_id
-  validates :day_id
-  validates :derively_fee_id
-  validates :prefecture_id
-  validates :status_id
+    validates :category_id
+    validates :day_id
+    validates :derively_fee_id
+    validates :prefecture_id
+    validates :status_id
   end
 
   validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }, inclusion: { in: (300..9_999_999), message: 'Out of setting range' }
